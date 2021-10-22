97891614ef2de074dde75e9e04aceb481ca2716f
Hal <hgrforrest@gmail.com>
2021-10-22 11:46:30 -0400

    added an h1 :muscle: :h1: :hello:

--- src/Hal.js
+++ src/Hal.js
@@ -1,7 +1,11 @@
+import { Link } from 'react-router-dom'
+
 const Hal = () => {
 	return (
 		<div class="hal">
+			<h1>Important work being done</h1>
 			<h2>Hal's crypt room</h2>
+			<Link to='https://www.halcodes.ca'>halcodes.ca</Link>
 		</div>
 	)
 }
