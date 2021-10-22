b3a9bb702512c92d0089eed542948c16df026e41
Hal <hgrforrest@gmail.com>
2021-10-22 11:47:49 -0400

    added madlib :mad: :lib: :comet:

--- src/Hal.js
+++ src/Hal.js
@@ -5,6 +5,7 @@ const Hal = () => {
 		<div class="hal">
 			<h1>Important work being done</h1>
 			<h2>Hal's crypt room</h2>
+			<p>I've become obsessed with _________ instead of designing my portfolio.  My portfolio is very far from being done, which was not at all the plan.  That said I've got really good at _________.</p>
 			<Link to='https://www.halcodes.ca'>halcodes.ca</Link>
 		</div>
 	)
