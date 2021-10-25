25bbd7404dd1fc2d6148bebb0ab7be58a5ea1e2e
Steven Chen <steven.chen.121@gmail.com>
2021-10-25 12:34:28 -0400

    New branch commit.

--- src/components/Steven.js
+++ src/components/Steven.js
@@ -2,7 +2,7 @@ const Steven = () => {
 	return (
 		<div className="steven">
 			<h2>Steven's crypt room</h2>
-			<p>Oh here we go again!</p>
+			<p>Check out my portfolio at <a href="https://stevenchen.ca" rel="noopener noreferrer">stevenchen.ca</a></p>
 		</div>
 	)
 }
