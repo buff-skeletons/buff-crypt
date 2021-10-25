325ed49dc42d451fc70088c524e8ebd6b09802f2
Maxim Belomestnov <maximbel@Maxims-MacBook-Air.local>
2021-10-25 12:43:37 -0400

    Just here for my daily commit

--- src/components/Maxim.js
+++ src/components/Maxim.js
@@ -1,8 +1,12 @@
 const Maxim = () => {
-	return (
-		<div className="maxim">
-			<h2>Maxim's crypt room</h2>
-		</div>
-	)
-}
-export default Maxim
+  return (
+    <div className="maxim">
+      <h2>Maxim's crypt room</h2>
+      <p>
+        Here for my daily commit. Here is my portfolio:
+        <a href="https://www.maximbelomestnov.com/">www.maximbelomestnov.com</a>
+      </p>
+    </div>
+  );
+};
+export default Maxim;
