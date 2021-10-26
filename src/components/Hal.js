81f62a18f286ae6c213723183f6c668eda68255c
Hal <hgrforrest@gmail.com>
2021-10-26 12:06:01 -0400

    modified my component, added form :form: :square: :circle:

--- src/components/Hal.js
+++ src/components/Hal.js
@@ -1,6 +1,16 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 const Hal = () => {
+	const [madLibProcrast, setMadLibProcrast] = useState('looking at this website')
+	const [madLibPrimary, setMadLibPrimary] = useState('portfolio')
+
+	const handleChangePro = (event) => {
+		setMadLibProcrast(event.target.value)
+	}
+	const handleChangePri = (event) => {
+		setMadLibPrimary(event.target.value)
+	}
+
 	useEffect(() => {
 		console.log("You checked the console, well done!");
 	}, [])
@@ -8,7 +18,15 @@ const Hal = () => {
 		<div className="hal">
 			<h1>Important work being done</h1>
 			<h2>Hal's crypt room</h2>
-			<p>I've become obsessed with _________ instead of designing my portfolio.  My portfolio is very far from being done, which was not at all the plan.  That said I've got really good at _________.</p>
+			<div className="madLibWrapper">
+				<form onSubmit={() => { }}>
+					<label htmlFor="madLibPrimary">Work activity</label>
+					<input type="text" name="madLibPrimary" id="madLibPrimary" value={madLibPrimary} onChange={handleChangePri} />
+					<label htmlFor="madLibProcrast">Procrastination activity</label>
+					<input type="text" name="madLibProcrast" id="madLibProcrast" value={madLibProcrast} onChange={handleChangePro} />
+				</form>
+				<p>I've become obsessed with {madLibProcrast} instead of working on my {madLibPrimary}.  My {madLibPrimary} is very far from being done, which was not at all the plan.  That said, I've gotten really good at {madLibProcrast}.</p>
+			</div>
 			<a href='https://www.halcodes.ca' target="_blank" rel="noopener noreferrer">halcodes.ca</a>
 			<p>Dark mode is the best mode</p>
 			<h1>Always end with an h1</h1>
