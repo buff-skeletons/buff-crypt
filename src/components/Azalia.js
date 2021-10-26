c635e59a66b6878bf1af94e1c3a274463611eb56
azalialoe <azalia.loeciano@gmail.com>
2021-10-26 17:18:43 -0400

    azalia added some stuffs (mainly meme) :lipstick:

--- src/components/Azalia.js
+++ src/components/Azalia.js
@@ -1,7 +1,12 @@
+import MathMeme from '../images/80b.jpg'
+
 const Azalia = () => {
 	return (
 		<div className="azalia">
 			<h2>Azalia's crypt room</h2>
+			<h1>THE ONE AND ONLY H1</h1>
+			<img src={MathMeme} alt="math meme" />
+			<p>🐈 I love cats so much, especially mine, but also, like, in general. 🐈 </p>
 		</div>
 	)
 }
