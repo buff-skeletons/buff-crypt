663b0683f434cc6a206a920d7fe0ee1bd23a26cd
tasnia <tasnianabila1@gmail.com>
2021-10-29 15:33:36 -0400

    added initial info to /tasnia

--- src/components/Tasnia.js
+++ src/components/Tasnia.js
@@ -2,6 +2,8 @@ const Tasnia = () => {
 	return (
 		<div className="tasnia">
 			<h2>Tasnia's crypt room</h2>
+			<h3>Checkout my portfolio: <a href="https://www.tasnianabila.com/" targer="_blank">www.tasnianabila.com</a> </h3>
+			<p>Totally awesome idea will be added soon</p>
 		</div>
 	)
 }
