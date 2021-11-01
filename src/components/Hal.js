f3370487609ec4f2cfb9cc7dc4aefdee118a73fa
Hal <hgrforrest@gmail.com>
2021-11-01 15:26:57 -0400

    added darkmode toggle to my component :dark: :light: :darkness: :comet:

--- src/components/Hal.js
+++ src/components/Hal.js
@@ -1,8 +1,9 @@
 import { useEffect, useState } from "react"
 
 const Hal = () => {
-	const [madLibProcrast, setMadLibProcrast] = useState('looking at this website')
-	const [madLibPrimary, setMadLibPrimary] = useState('portfolio')
+	const [madLibProcrast, setMadLibProcrast] = useState('adding a dark mode toggle')
+	const [madLibPrimary, setMadLibPrimary] = useState('job application process')
+	const [darkMode, setDarkMode] = useState(true)
 
 	const handleChangePro = (event) => {
 		setMadLibProcrast(event.target.value)
@@ -11,11 +12,23 @@ const Hal = () => {
 		setMadLibPrimary(event.target.value)
 	}
 
+	const toggleDarkMode = () => {
+		if (darkMode) {
+			setDarkMode(false)
+		}
+		else {
+			setDarkMode(true)
+		}
+	}
+
 	useEffect(() => {
 		console.log("You checked the console, well done!");
 	}, [])
+
+	console.log("Hal component did something");
+
 	return (
-		<div className="hal">
+		<div className={`hal ` + (darkMode ? 'dark' : 'light')}>
 			<h1>Important work being done</h1>
 			<h2>Hal's crypt room</h2>
 			<div className="madLibWrapper">
@@ -28,7 +41,8 @@ const Hal = () => {
 				<p>I've become obsessed with {madLibProcrast} instead of working on my {madLibPrimary}.  My {madLibPrimary} is very far from being done, which was not at all the plan.  That said, I've gotten really good at {madLibProcrast}.</p>
 			</div>
 			<a href='https://www.halcodes.ca' target="_blank" rel="noopener noreferrer">halcodes.ca</a>
-			<p>Dark mode is the best mode</p>
+			<p>{darkMode ? 'Dark' : 'Light'} mode is the best mode</p>
+			<button onClick={toggleDarkMode}> </button>
 			<h1>Always end with an h1</h1>
 		</div>
 	)
