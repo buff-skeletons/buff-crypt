c5b9ea48c8baaf92c1c70ac961880f3c2b01f011
Maxim Belomestnov <maximbel@Maxims-MacBook-Air.local>
2021-11-09 19:02:26 -0500

    Your new alarm clock

--- src/components/Azalia.js
+++ src/components/Azalia.js
@@ -1,14 +1,14 @@
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 
 const Azalia = () => {
-	return (
-		<div className="azalia">
-			<h2>Azalia's crypt room</h2>
-			<h1>MERRY CHRISTMAS</h1>
-			<h1>SECOND H1 AS AN EARLY CHRISTMAS GIFT</h1>
-			<ReactPlayer url="https://youtu.be/yXQViqx6GMY"/>
-			<p>🎄🎄🎄🎄🎄🎄 </p>
-		</div>
-	)
-}
-export default Azalia
+  return (
+    <div className="azalia">
+      <h2>Azalia's crypt room</h2>
+      <h1>MERRY CHRISTMAS</h1>
+      <h1>SECOND H1 AS AN EARLY CHRISTMAS GIFT</h1>
+      <ReactPlayer url="https://youtu.be/yXQViqx6GMY" />
+      <p>🎄🎄🎄🎄🎄🎄 </p>
+    </div>
+  );
+};
+export default Azalia;
--- src/components/Maxim.js
+++ src/components/Maxim.js
@@ -1,3 +1,5 @@
+import ReactPlayer from "react-player";
+
 const Maxim = () => {
   return (
     <div className="maxim">
@@ -6,6 +8,14 @@ const Maxim = () => {
         Here for my daily commit. Here is my portfolio:
         <a href="https://www.maximbelomestnov.com/">www.maximbelomestnov.com</a>
       </p>
+
+      <div className="alarmClockRecommendation">
+        <h3>
+          Anyone looking for a new alarm clock that will get your ass up every
+          morning, you're welcome.
+        </h3>
+        <ReactPlayer url="https://www.youtube.com/watch?v=enYdAxVcNZA" />
+      </div>
     </div>
   );
 };
